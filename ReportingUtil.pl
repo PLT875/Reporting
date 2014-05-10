8abf67092612bbcb1fb029e409870abfb5160338
Peter Tran <peter.tran@uclmail.net>
2014-05-10 20:59:15 +0100

    Matrix

--- ReportingUtil.pl
+++ ReportingUtil.pl
@@ -1,8 +1,7 @@
-#!/usr/local/bin/perl
-
 use strict;
 use warnings;
 use POSIX qw(strftime);
+use Data::Dumper;
 
 # Online Resources
 # ----------------
@@ -10,18 +9,46 @@ use POSIX qw(strftime);
 # http://www.tutorialspoint.com/perl/perl_directories.htm
 
 
-#&create_and_write_csv();
-&make_directory(strftime("%d-%m-%Y_%H:%M:%S", localtime(time)));
+
+#&make_directory(strftime("%d-%m-%Y_%H:%M", localtime(time)));
+
+my @matrix;
+$matrix[0][0] = 'Name';
+$matrix[0][1] = 'Occupation';
+$matrix[0][2] = 'Skills';
+$matrix[1][0] = 'Peter Tran';
+$matrix[1][1] = 'Software Developer';
+$matrix[1][2] = 'Java; Perl; PHP';
+$matrix[2][0] = 'Joe Bloggs';
+$matrix[2][1] = 'Tester';
+
+&create_and_write_csv(@matrix);
+
 
 sub create_and_write_csv {
+	my(@matrix) = @_;
 	my $directory = "./Reports";
 	chdir($directory) or die "Could not go to $directory\n";
-	my $filename = strftime("%d-%m-%Y_%H:%M:%S", localtime(time));
+	my $filename = strftime("%d-%m-%Y_%H-%M-%S", localtime(time));
 	open(FH, ">$filename.csv") or die "Error: Can't open file $filename.csv\n";
-	print FH "Peter Tran, 26, UCL \n";
-	print FH "Joe Bloggs, 28, King's College London \n";
-	close FH; 
-	return;
+	foreach my $row(@matrix){
+		my $current_column_no = 0;
+		my $last_column_no_current_row = scalar(@{ $row }) - 1;
+		foreach my $column(@$row){
+			# Get the field value and write to the csv.
+			print FH $column;
+			if($current_column_no < $last_column_no_current_row){
+				print FH ",";
+				$current_column_no++;
+			} else {
+				print FH "\n";
+				# End of current row.
+			}	
+		}			
+	}
+
+	close(FH);
+	return;	
 }
 
 sub make_directory {
@@ -29,3 +56,4 @@ sub make_directory {
 	mkdir($directory, 0775) or die "Could not create directory $directory\n";
 	return;
 }
+
